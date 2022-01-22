279d7adb242b2e427172b4c2456ba5ebe7f8dbfc
Guilherme Sabino de Freitas Gouveia <guilherme_sabino@yahoo.com.br>
2022-01-22 01:33:57 -0300

    Menu behaviours
    Added logic to menu controller reads PuzzlesSO and generate Puzzle Views with listener to their specific scene

---
+++ Assets/Game/Scripts/Editor/PuzzleSOEditor.cs
@@ -0,0 +1,79 @@
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Game.ScriptableObjects.Puzzles;
+
+// referencia
+// https://github.com/UnityTechnologies/open-project-1/blob/devlogs/2-scriptable-objects/UOP1_Project/Assets/Scripts/Editor/GameSceneSOEditor.cs
+
+namespace EditorScritps
+{
+    [CustomEditor(typeof(PuzzleSO), true)]
+    public class PuzzleSOEditor : Editor
+    {
+        private const string NO_SCENES_WARNING = "There is no Scene associated to this location yet. Add a new scene with the dropdown below";
+        private GUIStyle _headerLabelStyle;
+        private static readonly string[] _excludedProperties = { "m_Script", "sceneName" };
+
+        private string[] _sceneList;
+        private PuzzleSO _gameSceneInspected;
+
+        private void OnEnable()
+        {
+            _gameSceneInspected = target as PuzzleSO;
+            PopulateScenePicker();
+        }
+
+        public override void OnInspectorGUI()
+        {
+            EditorGUILayout.Space();
+            DrawScenePicker();
+            DrawPropertiesExcluding(serializedObject, _excludedProperties);
+        }
+
+        private void DrawScenePicker()
+        {
+            var sceneName = _gameSceneInspected.SceneName;
+            EditorGUI.BeginChangeCheck();
+            var selectedScene = _sceneList.ToList().IndexOf(sceneName);
+
+            if (selectedScene < 0)
+            {
+                EditorGUILayout.HelpBox(NO_SCENES_WARNING, MessageType.Warning);
+            }
+
+            selectedScene = EditorGUILayout.Popup("Scene", selectedScene, _sceneList);
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RecordObject(target, "Changed selected scene");
+                _gameSceneInspected.SceneName = _sceneList[selectedScene];
+                MarkAllDirty();
+            }
+        }
+
+        /// <summary>
+        /// Populates the Scene picker with Scenes included in the game's build index
+        /// </summary>
+        private void PopulateScenePicker()
+        {
+            var sceneCount = SceneManager.sceneCountInBuildSettings;
+            _sceneList = new string[sceneCount];
+            for (int i = 0; i < sceneCount; i++)
+            {
+                _sceneList[i] = Path.GetFileNameWithoutExtension(SceneUtility.GetScenePathByBuildIndex(i));
+            }
+        }
+
+        /// <summary>
+        /// Marks scenes as dirty so data can be saved
+        /// </summary>
+        private void MarkAllDirty()
+        {
+            EditorUtility.SetDirty(target);
+            EditorSceneManager.MarkAllScenesDirty();
+        }
+    }
+}
---
+++ Assets/Game/Scripts/Menu/PuzzleView.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Game.ScriptableObjects.Puzzles;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PuzzleView : MonoBehaviour
+{
+
+    public Action<string> OnClickButton;
+    private PuzzleSO _data;
+
+    [Header("View")]
+    [SerializeField]
+    private TextMeshProUGUI _puzzleTitle;
+
+    [SerializeField]
+    private GameObject dificultyContainer;
+
+    [SerializeField]
+    private Button uibutton;
+
+    public void SetData(PuzzleSO data)
+    {
+        this._data = data;
+
+        _puzzleTitle.text = _data?.puzzleTitle;
+
+        SetDificulty(_data.dificulty);
+
+        uibutton.onClick.AddListener(ClickButton);
+    }
+
+    private void ClickButton()
+    {
+        OnClickButton?.Invoke(_data.SceneName);
+    }
+
+    private void SetDificulty(int dificulty)
+    {
+        for (int i = 0; i < dificulty; i++)
+        {
+            dificultyContainer.transform.GetChild(i).gameObject.SetActive(true);
+        }
+    }
+}
