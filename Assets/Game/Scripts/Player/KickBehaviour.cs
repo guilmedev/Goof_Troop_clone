c5f7bc6d08337d724af385cab9d8e281ca970349
Guilherme Sabino de Freitas Gouveia <guilherme_sabino@yahoo.com.br>
2022-03-16 14:17:24 -0300

    FIx Kick for mobile joystick
    CalibrateKick to values from joystick

--- Assets/Game/Scripts/Player/KickBehaviour.cs
+++ Assets/Game/Scripts/Player/KickBehaviour.cs
@@ -14,11 +14,20 @@ public class KickBehaviour : MonoBehaviour, IKickBehaviour
     public void DoKick(Vector2 direction, GameObject kickableObject)
     {
         if (kickableObject != null)
-        {           
-            if (direction.x != 0 && direction.y != 0) return;
-
-            kickableObject.GetComponent<IKickable>().Kick(direction, this);
+        {
+            kickableObject.GetComponent<IKickable>().Kick(CalibrateKick(direction), this);
         }
+    }
+    private Vector2 CalibrateKick(Vector2 direction)
+    {
+        Vector2 calibratedDirection;
+
+        calibratedDirection.x = (Mathf.RoundToInt(direction.x * 10)) / 10;
+        calibratedDirection.y = (Mathf.RoundToInt(direction.y * 10)) / 10;
+
+        //Round to next tenth(decimal)
+        //Only accept values 1, -1 or 0 to avoid diagonal movements
+        return calibratedDirection;
     }
 
     public void KickFailed()
